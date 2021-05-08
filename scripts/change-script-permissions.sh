017851bb99e3634082250001a476068572fbdbf3
seonghyeoncho <chosh040@naver.com>
2021-05-08 17:24:46 +0900

    Github Action 구축

---
+++ scripts/change-script-permissions.sh
@@ -0,0 +1,5 @@
+cd /home/ec2-user/eatsroad
+
+sudo chown -R ec2-user:ec2-user /home/ec2-user/eatsroad
+chmod 777 /home/ec2-user/eatsroad
+chmod 777 /home/ec2-user/eatsroad/*/**
---
+++ scripts/run-application.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npx kill-port 3000
+cd /home/ec2-user/eatsroad
+rm -rf ~/log/eatsroad.log
+rm -rf ~/log/eatsroad-error.log
+yarn install
+yarn build
+node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
