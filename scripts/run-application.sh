ff3403cdfd7ed0b8c72918a189c65d4472ecdb01
seonghyeoncho <chosh040@naver.com>
2021-05-09 17:26:06 +0900

    Test

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -4,4 +4,5 @@ kill -9 5906
 cd /home/ec2-user/eatsroad
 rm -rf ~/log/eatsroad.log
 rm -rf ~/log/eatsroad-error.log
+mkdir testDir
 # node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
