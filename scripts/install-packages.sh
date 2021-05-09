6350c0a294849542c27fa28ec16dcaa19615d630
seonghyeoncho <chosh040@naver.com>
2021-05-09 20:26:02 +0900

    Test

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 cd /home/ec2-user/eatsroad &&
-
-yarn install &&
+yarn install
 yarn build
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
