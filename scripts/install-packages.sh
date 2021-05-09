83363d8b8ce218d1f7968686149e8fab4fe0e8ac
seonghyeoncho <chosh040@naver.com>
2021-05-09 16:57:10 +0900

    Test

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/ec2-user/eatsroad
+cd /home/ec2-user/eatsroad &&
 yarn install
--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -3,7 +3,7 @@
 kill -9 5906
 cd /home/ec2-user/eatsroad
 touch testFile
-rm -rf ~/log/eatsroad.log
-rm -rf ~/log/eatsroad-error.log
+rm -rf ~/log/eatsroad.log &&
+rm -rf ~/log/eatsroad-error.log &&
 yarn build &&
-yarn start ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
+node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
