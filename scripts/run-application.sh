a7be556d142d72fd091d157526f97f0f17ea7371
seonghyeoncho <chosh040@naver.com>
2021-05-09 18:17:18 +0900

    Test

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 fuser -k -n tcp 3000
-cd /home/ec2-user/eatsroad
 rm -rf ~/log/eatsroad.log
 rm -rf ~/log/eatsroad-error.log
-echo "A"
+cd /home/ec2-user/eatsroad &&
 node ./dist/main.js 
 # >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
