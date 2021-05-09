fd6c764c4504834500758a19ab01ac10f196a225
seonghyeoncho <chosh040@naver.com>
2021-05-09 17:31:26 +0900

    Test

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -5,4 +5,5 @@ cd /home/ec2-user/eatsroad
 rm -rf ~/log/eatsroad.log
 rm -rf ~/log/eatsroad-error.log
 mkdir testDir
-# node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
+node ./dist/main.js 
+# >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
