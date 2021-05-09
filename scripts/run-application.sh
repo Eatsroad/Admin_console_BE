3c8679e8f4b774e246353bf46ebd44b191d32a2e
seonghyeoncho <chosh040@naver.com>
2021-05-09 21:03:01 +0900

    Add .log

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -5,6 +5,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 fuser -k -n tcp 3000
-# rm -rf ~/log/eatsroad.log && rm -rf ~/log/eatsroad-error.log
+rm -rf ~/log/eatsroad.log
+rm -rf ~/log/eatsroad-error.log
+
 cd /home/ec2-user/eatsroad
 node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
