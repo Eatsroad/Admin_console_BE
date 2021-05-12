d7fcf2a5ae93ec3573bb6abbcd6d019896305366
seonghyeoncho <chosh040@naver.com>
2021-05-12 17:13:38 +0900

    change command

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -9,4 +9,4 @@ rm -rf ~/log/eatsroad.log
 rm -rf ~/log/eatsroad-error.log
 
 cd /home/ec2-user/eatsroad
-node ./dist/main.js >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
+yarn start >> /home/ec2-user/log/eatsroad.log 2>> /home/ec2-user/log/eatsroad-error.log &
