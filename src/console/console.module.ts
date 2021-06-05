dece7ac1cdcc1acd0859a6629f4c5881ad4549b1
ohjinho <ohjinho7@gmail.com>
2021-06-05 19:17:39 +0900

    store를 console에 추가

--- src/console/console.module.ts
+++ src/console/console.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CategoryModule } from './category/category.module';
 import { EventModule } from './event/event.module';
 import { OptionModule } from './option/option.module';
+import { StoreModule } from './store/store.module';
 
 
 
 @Module({
-  imports: [CategoryModule,EventModule,OptionModule],
+  imports: [CategoryModule,EventModule,OptionModule, StoreModule],
   providers: []
 })
 export class ConsoleModule {}
