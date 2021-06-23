ba35fc52aebba3096b43a59c30ff6ec59b8452d7
SeoeunBae <enyoupark@gmail.com>
2021-06-23 12:02:11 +0900

    기본로직,테스트완성

--- src/console/menu/dtos/update-menu.dto.ts
+++ src/console/menu/dtos/update-menu.dto.ts
@@ -1,7 +1,19 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { IsNumber, IsOptional } from "class-validator";
+import { Category } from "src/entities/category/category.entity";
+import { EnableTime } from "src/entities/menu/enableTime.entity";
+import { OptionGroup } from "src/entities/option/optionGroup.entity";
 import { MenuCreateDto } from "./create-menu.dto";
 
 
 export class MenuUpdateDto extends PartialType(MenuCreateDto) {
+    @IsOptional()
+    categories: Category[];
+    
+    @IsOptional()
+    optionGroups:OptionGroup[];
 
+    @IsOptional()
+    @IsNumber()
+    enable_time:EnableTime;
 }
