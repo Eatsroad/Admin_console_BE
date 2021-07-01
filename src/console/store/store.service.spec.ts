b128b3c5f5d6f15b0de10aaf79220fc4c130c270
ohjinho <ohjinho7@gmail.com>
2021-07-01 22:57:01 +0900

    test코드 작성 1차완료

--- src/console/store/store.service.spec.ts
+++ src/console/store/store.service.spec.ts
@@ -1,55 +1,66 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { Store } from '../../../src/entities/store/store.entity';
-import { User } from '../../../src/entities/user/user.entity';
-import { createMemoryDB } from '../../../src/utils/connections/create-memory-db';
-import { Connection, Repository } from 'typeorm';
-import { StoreService } from './store.service';
-import { StoreCreateDto } from './dtos/create-store.dto';
-import { ConflictException, NotFoundException } from '@nestjs/common';
-import { BasicMessageDto } from '../../../src/common/dtos/basic-massage.dto';
-import { StoreUpdateDto } from './dtos/update-store.dto';
-import { stringify } from 'querystring';
-import { Menu } from 'src/entities/menu/menu.entity';
-import { Category } from 'src/entities/category/category.entity';
-import { OptionService } from '../option/option.service';
-import { OptionGroup } from 'src/entities/option/optionGroup.entity';
-import { EnableTime } from 'src/entities/menu/enableTime.entity';
+import { Test, TestingModule } from "@nestjs/testing";
+import { Store } from "../../../src/entities/store/store.entity";
+import { User } from "../../../src/entities/user/user.entity";
+import { createMemoryDB } from "../../../src/utils/connections/create-memory-db";
+import { Connection, Repository } from "typeorm";
+import { StoreService } from "./store.service";
+import { StoreCreateDto } from "./dtos/create-store.dto";
+import { ConflictException, NotFoundException } from "@nestjs/common";
+import { BasicMessageDto } from "../../../src/common/dtos/basic-massage.dto";
+import { StoreUpdateDto } from "./dtos/update-store.dto";
+import { stringify } from "querystring";
+import { Menu } from "../../../src/entities/menu/menu.entity";
+import { Category } from "../../../src/entities/category/category.entity";
+import { OptionService } from "../option/option.service";
+import { OptionGroup } from "../../../src/entities/option/optionGroup.entity";
+import { EnableTime } from "../../../src/entities/menu/enableTime.entity";
+import { Option } from "../../../src/entities/option/option.entity";
+import { extractUserId } from "../../../src/utils/auth/jwt-token-util";
 
-describe('StoreService', () => {
+describe("StoreService", () => {
   let storeService: StoreService;
   let connection: Connection;
-  let storeRepository : Repository<Store>;
+  let storeRepository: Repository<Store>;
 
-  const NAME = 'NAME';
-  const ADDRESS = 'ADDRESS';
-  const PHONE_NUMBER = '01012345667';
+  const NAME = "NAME";
+  const ADDRESS = "ADDRESS";
+  const PHONE_NUMBER = "01012345667";
   const TABLES = 45;
-  const USERID = "1234";
+  const USERID = null;
 
-  const saveStore = async () : Promise<Store>=>{
+  const saveStore = async (): Promise<Store> => {
     const savedStore = new Store();
     savedStore.setName = NAME;
     savedStore.setAddress = ADDRESS;
     savedStore.setPhone_number = PHONE_NUMBER;
     savedStore.setTables = TABLES;
-    return await storeRepository.save(savedStore)
+    savedStore.user_id = USERID;
+    return await storeRepository.save(savedStore);
   };
 
   beforeAll(async () => {
-    connection = await createMemoryDB([User,Store,Menu,Category,Option,OptionGroup, EnableTime]);
+    connection = await createMemoryDB([
+      User,
+      Store,
+      Menu,
+      Option,
+      Category,
+      OptionGroup,
+      EnableTime,
+    ]);
     storeRepository = await connection.getRepository(Store);
-    storeService = new StoreService(storeRepository)
+    storeService = new StoreService(storeRepository);
   });
 
-  afterAll(async() =>{
+  afterAll(async () => {
     await connection.close();
   });
 
-  afterEach(async () => {
-    await storeRepository.query("DELETE FROM stores");
-  });
-  
-  it('should be defined', () => {
+  // afterEach(async () => {
+  //   await storeRepository.query("DELETE FROM stores");
+  // });
+
+  it("should be defined", () => {
     expect(storeService).toBeDefined();
   });
 
@@ -59,7 +70,9 @@ describe('StoreService', () => {
     dto.address = ADDRESS;
     dto.phone_number = PHONE_NUMBER;
     dto.tables = TABLES;
-    const userid = USERID;
+    const userid = extractUserId(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImV4cCI6MTcxMTU0NTY2NywiaWF0IjoxNjI1MTQ1NjY3fQ.aQBsbk4xLhzC4_Qjvm--rm9XEtotbeL-1iKRbiyn5yU"
+    );
 
     const responseDto = await storeService.saveStore(dto, userid);
 
@@ -91,13 +104,16 @@ describe('StoreService', () => {
     dto.address = ADDRESS;
     dto.tables = TABLES;
     dto.phone_number = PHONE_NUMBER;
+    const userid = extractUserId(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImV4cCI6MTcxMTU0NTY2NywiaWF0IjoxNjI1MTQ1NjY3fQ.aQBsbk4xLhzC4_Qjvm--rm9XEtotbeL-1iKRbiyn5yU"
+    );
 
     try {
-      await storeService.saveStore(dto, 1234);
+      await storeService.saveStore(dto, userid);
     } catch (exception) {
       expect(exception).toBeInstanceOf(ConflictException);
     }
-  });  
+  });
 
   it("Should get store info correctly", async () => {
     let savedStore = new Store();
@@ -106,7 +122,7 @@ describe('StoreService', () => {
     savedStore.setTables = TABLES;
     savedStore.setPhone_number = PHONE_NUMBER;
     savedStore = await storeRepository.save(savedStore);
-  
+
     const response = await storeService.getStoreInfo(savedStore.getStore_id);
     expect(response.store_id).toBe(savedStore.getStore_id);
     expect(response.address).toBe(savedStore.getAddress);
@@ -129,7 +145,7 @@ describe('StoreService', () => {
     updateDto.name = "NEW_NAME";
     updateDto.address = "NEW_ADDRESS";
     updateDto.tables = 100;
-    updateDto.phone_number = "01087654321"
+    updateDto.phone_number = "01087654321";
 
     const response = await storeService.updateStoreInfo(
       savedStore.getStore_id,
