4a8e44b581a7cf349a56c40d0ad617d69f47d325
ohjinho <ohjinho7@gmail.com>
2021-07-06 14:57:48 +0900

    menu만드는중

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ConsoleModule } from './console/console.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, ConsoleModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, ConsoleModule, ClientModule],
   controllers: [],
   providers: [],
 })
--- src/client/menuboard/menuboard.controller.ts
+++ src/client/menuboard/menuboard.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Query } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CategoryService } from "src/console/category/category.service";
 import { MenuboardCategoryResponseDto } from "./dtos/menuboard-info.dto";
 import { MenuboardService } from "./menuboard.service";
@@ -9,8 +9,31 @@ import { MenuboardService } from "./menuboard.service";
 export class MenuboardController {
   constructor(private readonly menuBoardService: MenuboardService) {}
 
-  @Get()
+  @Get("/:storeId")
+  @ApiOperation({
+    summary: "카테고리 정보 API",
+    description: "해당 음식점의 모든 (음식)카테고리를 보여줍니다.",
+  })
+  @ApiResponse({
+    description: "해당 음식점의 모든 (음식)카테고리를 보여줍니다.",
+    type: MenuboardCategoryResponseDto,
+  })
   getAllCategory(
+    @Param("storeId", ParseIntPipe) storeId: number
+  ): Promise<MenuboardCategoryResponseDto[]> {
+    return this.menuBoardService.getCategoryByStoreId(storeId);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: "카테고리 - 음식 API",
+    description: "해당 카테고리에 해당하는 음식을 보여줍니다.",
+  })
+  @ApiResponse({
+    description: "해당 카테고리에 해당하는 음식을 보여줍니다.",
+    type: MenuboardCategoryResponseDto,
+  })
+  getCategoryByMenuId(
     @Query("storeId") storeId: number
   ): Promise<MenuboardCategoryResponseDto[]> {
     return this.menuBoardService.getCategoryByStoreId(storeId);
