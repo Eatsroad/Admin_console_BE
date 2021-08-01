8385f64f4dc6cab15e4025a71afcf96bf39ea362
SeoeunBae <enyoupark@gmail.com>
2021-08-01 21:03:06 +0900

    tokenbranch생성

--- src/utils/auth/jwt-token-util.ts
+++ src/utils/auth/jwt-token-util.ts
@@ -5,20 +5,23 @@ export const generateAccessToken = (userId: number): string => {
   return jwt.sign(
     {
       userId: userId,
-      exp: Math.floor(Date.now() / 1000) + 86400000,
+      exp: Math.floor(Date.now() / 1000) + 86400000
     },
     `${process.env.JWT_SERCET_KEY}`
   );
 }
+
 function checkExpDate(exp: number): void {
   try {
-    if (exp * 1000 < Date.now() + 86400000) {
+    if (exp * 1000 < Date.now() + 86400000)
+    {
       throw new UnauthorizedException("JWT Token has been expired.");
     }
   } catch (exception) {
     throw new UnauthorizedException("JWT Token is malformed.");
   }
 }
+
 export function extractUserId(token: string): number {
   try {
     const decodedToken = jwt.verify(token, `${process.env.JWT_SERCET_KEY}`) as {
